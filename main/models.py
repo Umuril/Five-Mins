42a88ff187c60b31177d511ff4c8aab6fefbf541
Umuril Lyerood <umuril.lyerood@gmail.com>
2023-06-19 16:35:36 +0200

    Register Job model on admin panels

--- main/models.py
+++ main/models.py
@@ -4,18 +4,22 @@ from djmoney.models.fields import MoneyField
 
 class Job(models.Model):
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
+    description = models.CharField(null=True, blank=True, max_length=255)
     
     requester = models.ForeignKey(User, on_delete=models.PROTECT, related_name='requests')
-    assigned_to = models.ForeignKey(User, on_delete=models.PROTECT, related_name='works')
-    status = models.IntegerField(null=False)
+    assigned_to = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT, related_name='works')
+    status = models.IntegerField(null=True, blank=True)
     
     request_start_time = models.DateTimeField()
     request_duration = models.DurationField()
-    request_location = models.CharField(max_length=255) # FIXME
-    request_price = MoneyField(max_digits=6, decimal_places=2, default_currency='EUR', editable=True)
+    request_location = models.CharField(null=True, blank=True, max_length=255) # FIXME
+    request_price = MoneyField(null=True, blank=True, max_digits=6, decimal_places=2, default_currency='EUR', editable=True)
     
-    stars = models.IntegerField(null=True)
+    stars = models.IntegerField(null=True, blank=True)
 
-    creation_time = models.DateTimeField(null=False, editable=False)
-    over_time = models.DateTimeField(null=True, editable=False)
+    creation_time = models.DateTimeField(null=False, editable=False, auto_now_add=True)
+    over_time = models.DateTimeField(null=True, blank=True, editable=False)
+
+    def __str__(self):
+        return self.title
+    
