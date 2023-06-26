a557bff75b164e4f9e71dfa8749cb5b71e6ec785
Umuril Lyerood <umuril.lyerood@gmail.com>
2023-06-26 13:19:19 +0200

    Add more Users info on admin panel

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from main.models import Hand, Profile
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from main.models import Hand
+
 
 class ProfileInline(admin.StackedInline):
     model = Profile
@@ -10,9 +12,45 @@ class ProfileInline(admin.StackedInline):
 
 
 class UserAdmin(BaseUserAdmin):
+    list_display = [
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "submits_count",
+        "requests_count",
+        "works_count",
+    ]
     inlines = [ProfileInline]
+
+    def submits_count(self, obj):
+        return obj.submits.count()
+
+    def works_count(self, obj):
+        return obj.works.count()
+
+    def requests_count(self, obj):
+        return obj.requests.count()
+
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
-admin.site.register(Hand)
+
+@admin.register(Hand)
+class HandAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "requester",
+        "status",
+        "request_date",
+        "request_start_time",
+        "request_end_time",
+        "assigned_to",
+        "request_stars",
+        "work_stars",
+        "submit_count",
+    ]
+
+    def submit_count(self, obj):
+        return obj.submits.count()
